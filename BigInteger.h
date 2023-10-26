182c9f2922af8b3b48db7c271e3d1547028f7cca
shashanksharma2003 <139097616+shashanksharma2003@users.noreply.github.com>
2023-10-26 18:46:19 +0530

    Update BigInteger.h

--- BigInteger.h
+++ BigInteger.h
@@ -13,35 +13,35 @@ struct BigInteger
     int length;
 };
 
-void sign_rev(struct node *head);
-struct BigInteger initialize(char *s);
+void sign_rev(struct node *);
+struct BigInteger initialize(char *);
 
 
-struct BigInteger add(struct BigInteger a,struct BigInteger b);
-struct BigInteger add_plus_plus(struct BigInteger a,struct BigInteger b,int sign);
-struct BigInteger add_minus_plus(struct BigInteger a,struct BigInteger b,int sign);
+struct BigInteger add(struct BigInteger ,struct BigInteger );
+struct BigInteger add_plus_plus(struct BigInteger ,struct BigInteger ,int );
+struct BigInteger add_minus_plus(struct BigInteger ,struct BigInteger ,int );
 
 
 
-struct BigInteger sub(struct BigInteger a,struct BigInteger b);
+struct BigInteger sub(struct BigInteger ,struct BigInteger );
 
-struct BigInteger mul(struct BigInteger a,struct BigInteger b);
-struct BigInteger multiply(struct BigInteger a , struct BigInteger b ,int sign);
+struct BigInteger mul(struct BigInteger ,struct BigInteger );
+struct BigInteger multiply(struct BigInteger  , struct BigInteger  ,int );
 
-struct BigInteger div1(struct BigInteger a , struct BigInteger b );
-struct BigInteger division(struct BigInteger a , struct BigInteger b , int sign , int choice);
-void div_add(struct node *p1 ,struct node *p2 ,struct node *p3 ,int *l );
-void div_sub(struct node *p1 ,struct node *p2 ,struct node *p3 );
-int compare(struct BigInteger h1 , struct BigInteger h2);
-int compare_sub(struct node *h1 , struct node *h2);
+struct BigInteger div1(struct BigInteger , struct BigInteger  );
+struct BigInteger division(struct BigInteger  , struct BigInteger  , int  , int );
+void div_add(struct node * ,struct node * ,struct node * ,int * );
+void div_sub(struct node * ,struct node * ,struct node * );
+int compare(struct BigInteger  , struct BigInteger );
+int compare_sub(struct node * , struct node *);
 
 
-void display(struct BigInteger c);
-void print_ll(struct node *head);
+void display(struct BigInteger );
+void print_ll(struct node *);
 
-void turnicate(struct BigInteger dev);
+void turnicate(struct BigInteger );
 
-void dump(struct BigInteger c);
+void dump(struct BigInteger );
 
-struct BigInteger mod(struct BigInteger a , struct BigInteger b);
+struct BigInteger mod(struct BigInteger  , struct BigInteger );
 //struct BigInteger mod_sub(struct BigInteger a , struct BigInteger b);
